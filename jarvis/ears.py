dea80617318cd9c0a28e37de7e1b869f7c179bd2
MrMallIronmaker <725mrm@gmail.com>
2015-05-19 15:53:38 -0500

    Ears module now can translate speech to text accurately

--- jarvis/ears.py
+++ jarvis/ears.py
@@ -1,25 +1,40 @@
-import speech_recognition as sr
-import pyaudio
+from speech_recognition import Recognizer, Microphone
+from pyaudio import PyAudio
+from utils import vprint
+
+from time import sleep
 
 def ears_setup():
-	p = pyaudio.PyAudio()
+	p = PyAudio()
 	count = p.get_device_count()
 	device = [i for i in range(count) if 'Logitech' in p.get_device_info_by_index(i)['name']][0]
 
-	source = sr.Microphone(device_index=device)
-	source.CHUNK=256
+	source = Microphone(device_index=device)
+	# yup, I'm playing with the internals of this class.
+	source.CHUNK=512
 	source.RATE=8000
+	source.CHANNELS = 1
 	try:
 		source.__enter__()
+		source.stream.stop_stream()
 	except:
+		vprint(1, "Microphone initialization failed.")
 		source.__exit__()
 
 	return source
 
 def recognize(source):
-	r = sr.Recognizer()
+	r = Recognizer()
+	source.stream.start_stream()
 	audio = r.listen(source)
+	source.stream.stop_stream()
+	vprint(4, "Finished recording.")
 	try:
 	    vprint(0, "You said " + r.recognize(audio))
 	except LookupError:
  	    vprint(0, "Could not understand audio")
+
+if __name__ == "__main__":
+	r, source = ears_setup()
+	#sleep(1)
+	recognize(r, source)
